3eba85b265b43e689498dcb2c0a1e048d4ef1f58
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-05-03 14:43:05 +0530

    added console

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -4,6 +4,7 @@ export const getRooms = async (req, res) => {
     try {
         const [rows] = await pool.execute('SELECT id, room_name FROM rooms ORDER BY room_name');
         res.json(rows);
+        console.log(rows)
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
